d30b0c59a731c0498caae4ceeef1923e6e2af17d
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-12-27 19:20:07 +0530

    disaled ssl warnings

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import urllib3
 from html.parser import HTMLParser
 from Token import getToken
 from SessionID import getSessionID
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 session = requests.session()
-
 vahan_url = "https://vahaninfos.com/vehicle-details-by-number-plate/"
 
 headers = {"Sec-Ch-Ua": "\"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
